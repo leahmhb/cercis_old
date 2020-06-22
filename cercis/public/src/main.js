6e015b02b7f522ac61eac49a5f671fdf46e81e51
Leah B <leah.mhb@gmail.com>
2020-06-22 15:43:17 -0400

    custom styles and fonts

--- cercis/public/src/main.js
+++ cercis/public/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/styles/custom-bootstrap-overrides.scss'
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap-vue/src/index.scss'
+
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
